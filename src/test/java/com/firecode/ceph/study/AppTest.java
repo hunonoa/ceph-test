230e51f7ec93f3c17ec0c3f3166552470eff0d22
yuage <133097618+hunonoa@users.noreply.github.com>
2023-05-12 11:48:59 +0800

    Update AppTest.java

--- src/test/java/com/firecode/ceph/study/AppTest.java
+++ src/test/java/com/firecode/ceph/study/AppTest.java
@@ -13,10 +13,7 @@ public class AppTest
     /**
      * Create the test case
      *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
+ 
         super( testName );
     }
 
